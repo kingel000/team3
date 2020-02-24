ff82d35dec6a5b419ee9ca04c24ddedd3770e641
MSI <MSI@DESKTOP-92TG779>
2020-02-24 22:31:06 +0900

    admin 설정

---
+++ mainProject/src/main/java/main/project/admin/adminController.java
@@ -0,0 +1,11 @@
+package main.project.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("adminController")
+@RequestMapping(value="/admin")
+public class adminController {
+	//* package  -> "main.project.admin
+	//localhost:8080/web/admin/*.mdo
+}
